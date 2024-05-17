ac64fdcd586c25ca7aff74fd8ecf6fa48d2ccb18
ErudyWu <wuzilong0208@gmail.com>
2024-05-17 16:10:22 -0400

    Add files via upload

---
+++ AO2/src/tests/TestCalculator.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestCalculator {
+
+    @Test
+    public void testCalculator() {
+        assertTrue(true);
+    }
+
+}
